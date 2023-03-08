b9b4adeab1c8ad48f4de99ce5651f8d69474805f
emShalaby <medo.dice@gmail.com>
2023-03-08 07:33:53 +0200

    font-size adjustments

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+// --------constants----
 const form=document.querySelector('form');
-const password=document.querySelector('#pass')
-const confirmPass=document.querySelector("#confirmPass")
+const password=document.querySelector('#pass');
+const confirmPass=document.querySelector("#confirmPass");
+const passError=document.querySelector(".passError");
 
+// ------functions------
 function checkPassword(){
 
-
-    if (password.value!=confirmPass.value) {
+    if (password.value!=confirmPass.value && password.value!='') {
     password.style.setProperty("--borderColor","red");
     confirmPass.style.setProperty('--borderColor','red');
+    passError.style.display='block';
+    
     }
     else{
         password.style.setProperty("--borderColor",'blue');
         confirmPass.style.setProperty('--borderColor','blue')
+        passError.style.display='none';
     }
 }
+// ------events------
 password.addEventListener('input',checkPassword);
 confirmPass.addEventListener('input',checkPassword);
